5dff36f90ca8104d16708a9c78bc5eb11f5a89d6
0xakash9 <devakash256@gmail.com>
2024-03-26 23:30:54 +0530

    scrape yc new jobs

--- main.go
+++ main.go
@@ -1,26 +1,60 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
+	"fmt"
+	"log"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gocolly/colly"
 )
 
-func main(){
-
-  r:= chi.NewRouter();
-  r.Use(middleware.Logger)
-  r.Get("/",func(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type","application/json");
-     encoder :=	json.NewEncoder(w);
-	 encoder.Encode("Rest API");
-  })
-
-   http.ListenAndServe(":4000",r);
-
+type Job struct {
+ name string;
+ logo string;
+ role string
+//  location string
 }
 
+var ycJobs []Job
+
+func main() {
+
+	// r:= chi.NewRouter();
+	// r.Use(middleware.Logger)
+	// r.Get("/",func(w http.ResponseWriter, r *http.Request){
+	//   w.Header().Set("Content-Type","application/json");
+	//    encoder :=	json.NewEncoder(w);
+	//    encoder.Encode("Rest API");
+	// })
+  
+	//  http.ListenAndServe(":4000",r);
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.ycombinator.com","www.ycombinator.com/jobs"),
+	)
+	
+	
+
+	c.OnHTML("ul.space-y-2.overflow-hidden > li", func(e *colly.HTMLElement) {
+		post := Job{};
+
+		post.name = e.ChildText("span.block.font-bold");
+        post.logo = e.ChildAttr("img", "src");
+		post.role = e.ChildText("div>a.font-semibold.text-linkColor")
+		
+		// fmt.Println("location",e.ChildText("div.flex.flex-row.flex-wrap.justify-center"))
+
+        ycJobs = append(ycJobs, post)
+	})
+	
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting:", r.URL.String())
+	})
+	
+	if err := c.Visit("https://www.ycombinator.com/jobs"); err != nil {
+		log.Fatal("Error visiting initial URL:", err)
+	}
+
+	fmt.Println(ycJobs)
+}
 
 
